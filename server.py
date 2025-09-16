37e8f705fe37af7bd8b4ea8d0117335c7b3d02cc
ivanrj7j <ivanrj7j@gmail.com>
2025-09-16 22:25:45 +0530

    added price fetching

--- server.py
+++ server.py
@@ -2,13 +2,15 @@ import os
 from flask import Flask, request, jsonify, render_template
 from dotenv import load_dotenv
 from ai import GeminiCropRecommender
+import requests
 
 # Load environment variables from .env file
 load_dotenv()
 
 API_KEY = os.getenv("GEMINI_API_KEY")
-if not API_KEY:
-    raise RuntimeError("GEMINI_API_KEY not found in environment variables. Please set it in your .env file.")
+GOV_KEY = os.getenv("SEED_API_KEY")
+if not API_KEY or not GOV_KEY:
+    raise RuntimeError("API_KEY not found in environment variables. Please set it in your .env file.")
 
 
 
@@ -47,6 +49,35 @@ def recommend_crops():
             return jsonify({"error": f"Unexpected error: {e}"}), 500
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+@app.route('/getPrices', methods=['POST'])
+def getPrices():
+    searchOptions = [
+        "Paddy",
+        "Maize",
+        "Moong",
+        "Millet",
+    ]
+    data = request.get_json()
+    if "crop" not in data or data["crop"] not in searchOptions:
+        return jsonify({"error": "Invalid crop"}), 403
+    
+    print(data)
+    
+    url = f"https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key={GOV_KEY}&format=json&limit=50&filters%5Bcommodity%5D={data["crop"]}"
+    fetchData = requests.get(url).json()
+    totalRecords = int(fetchData["total"])
+
+    if totalRecords == 0:
+        return jsonify({"error": "data not found"}), 404
+    
+    minPrice, modPrice, maxPrice = 0, 0, 0
+    for record in fetchData["records"]:
+        minPrice += int(record["min_price"])
+        modPrice += int(record["modal_price"])
+        maxPrice += int(record["max_price"])
+
+    return jsonify({"min":minPrice/totalRecords, "mod":modPrice/totalRecords, "max":maxPrice/totalRecords})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
